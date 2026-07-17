9ea384183377cf51512715e1eaedf496c249d7a1
vasi <61645055+vasimalaivasu@users.noreply.github.com>
2026-07-17 10:10:09 +0530

    day1 task.js

---
+++ task.js
@@ -0,0 +1,131 @@
+//Task - 1
+const company = "stackly";
+let employee_name=prompt("Enter the Employee name")
+let emp_id = "EMP101"
+
+console.log(company);
+console.log(employee_name);
+console.log(emp_Id);
+
+task - 2
+const college = " ABC Engnieering College ";
+var student_name = prompt("Enter the Student name");
+var course = prompt("Enter the course");
+
+console.log(college);
+console.log(student_name);
+console.log(course);
+
+task - 3
+const restaurant = "Food Park";
+let customerName = prompt("Enter Customer Name:");
+let foodName = prompt("Enter Food Name:");
+let quantity = 2;
+
+console.log(restaurant);
+console.log(customerName);
+console.log(foodName);
+console.log(quantity);
+
+task - 4 
+const hospital = "Apollo";
+let patientName = prompt("Enter Patient Name:");
+let tokenNumber = 25;
+
+console.log(hospital);
+console.log(patientName);
+console.log(tokenNumber);
+
+task - 5
+const theatre = "PVR Cinemas";
+let movieName = prompt("Enter Movie Name:");
+let customerName = prompt("Enter Customer Name:");
+let seatNumber = "A12";
+
+console.log(theatre);
+console.log(movieName);
+console.log(customerName);
+console.log(seatNumber);
+
+task - 6
+const shop = "Smart Mobiles";
+let customerName = prompt("Enter Customer Name:");
+let mobileModel = prompt("Enter Mobile Model:");
+let price = 25000;
+
+console.log(shop);
+console.log(customerName);
+console.log(mobileModel);
+console.log(price);
+
+task - 7
+const gym = "Fit Zone";
+let memberName = prompt("Enter Member Name:");
+let membershipFee = 1500;
+console.log(gym);
+console.log(memberName);
+console.log(membershipFee);
+
+task - 8
+const bank = "SBI";
+let userName = prompt("Enter Your Name:");
+console.log(userName);
+console.log(bank);
+
+task - 9
+const travels = "KPN Travels";
+let passengerName = prompt("Enter Passenger Name:");
+let seatNumber = "B10";
+console.log(travels);
+console.log(passengerName);
+console.log(seatNumber);
+
+task - 10
+const website = "Amazon";
+let productName = prompt("Enter Product Name:");
+let customerName = prompt("Enter Customer Name:");
+let price = 1200;
+console.log(website);
+console.log(productName);
+console.log(customerName);
+console.log(price);
+
+task - 11
+console.log("Registration Successful");
+
+task - 12
+console.error("Payment Failed");
+
+task - 13
+console.warn("Battery Low");
+
+task - 14
+console.log("Loading...");
+console.log("Welcome");
+console.clear();
+
+task - 15
+alert("Welcome to Amazon");
+
+task - 16
+let logout = confirm("Do you want to logout?");
+console.log(logout);
+
+task - 17
+let city = prompt("Enter your city:");
+console.log( city);
+
+task - 18 
+var product = "Laptop";
+product = "Mobile";
+var product = "Tablet";
+console.log(product);
+
+task - 19
+let salary = 25000;
+salary = 30000;
+console.log(salary);
+
+task - 20
+const company = "Google";
+console.log(company);
